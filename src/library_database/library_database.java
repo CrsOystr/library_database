ce1a3f53aeff3031e2c0c3a0219c43effb3e48f1
CrsOystr <nicmetz@jackmoves.com>
2015-03-31 16:52:07 -0600

    think im on the right track, got a few more queries instrttedd

--- src/library_database/library_database.java
+++ src/library_database/library_database.java
@@ -60,6 +60,7 @@ public class library_database {
 			    case 2:
 			    	break;
 			    case 3:
+			    	wait_list();
 			    	break;	
 			    case 4:
 			    	break;
@@ -141,6 +142,56 @@ public class library_database {
 	         }
 	}
 	
+	//Function for allowing users to be added to a waitlist
+	public static void wait_list(){
+		try{
+			String isbn = null;
+			boolean isbn_loop = true;
+			//this loop ensures the isbn exists in the database
+			while (isbn_loop)
+			{
+				System.out.println("Enter the isbn of the books to be waited for:");
+		    	isbn = in.nextLine();
+		    	String query = "SELECT * FROM BOOK_DIR where isbn = ?";
+		    	PreparedStatement query_statment = con.prepareStatement(query);
+		    	query_statment.setString(1, "" + isbn);
+		    	ResultSet rs1=query_statment.executeQuery();
+		    	if (rs1.next()){
+		    		isbn_loop = false;
+		    	}
+		    	else 
+		    	{
+		        	System.out.println("You can only wait for books that already exist in the library database.");
+		        	System.out.println("Hit enter to return to the main menu and add information about this book to the database.");
+		        	in.nextLine();
+		        	return;
+		    	}
+			}
+			System.out.println("Enter userID to be added to waitlist:");
+	    	String user_id = in.nextLine();
+			
+	    	String wait_query = "INSERT INTO WAIT_LIST(user_id, isbn, wait_since) "
+	    			+ "VALUES(?,?,?)";
+	    	PreparedStatement wait_state = con.prepareStatement(wait_query);
+	    	wait_state.setString(1, user_id);
+	    	wait_state.setString(2, isbn);
+	    	long time = System.currentTimeMillis();
+	    	java.sql.Date date = new java.sql.Date(time);
+	    	wait_state.setDate(3,date);
+	    	wait_state.executeUpdate();
+			
+			
+			
+		}
+		catch(Exception e){
+    		System.out.println("Something went wrong adding to waitlist");
+		}
+		//Forces user to hit enter to return home
+		System.out.println("Please hit enter to return to the main menu");
+    	in.nextLine();
+    	return;
+	}
+	
 	//FUNCTION FOR ADDING COPIES OF BOOKS
 	public static void add_books(){
 		try{
@@ -176,21 +227,37 @@ public class library_database {
 	    	String isbn_query = "SELECT MAX(copy_number) FROM BOOK_STOCK WHERE isbn = ? GROUP BY isbn";
 	    	PreparedStatement query2 = con.prepareStatement(isbn_query);
 	    	query2.setString(1, "" + isbn);
-	    	
-	    	
-
-    		System.out.println(query2);
 	    	ResultSet rs2=query2.executeQuery();
-    		System.out.println(query2);
-	    	rs2.next();
-	    	int user_id = rs2.getInt(1) + 1;
-			
+    		int copy_start = 1;
+	    	if(rs2.next()){
+	    		copy_start = rs2.getInt(1) + 1;
+	    	}
+	    	for (int i = copy_start; i < copy_start + copies; i++)
+	    	{
+		    	String book_query = "INSERT INTO BOOK_STOCK(isbn, copy_number, location) "
+		    			+ "VALUES(?,?,?)";
+		    	PreparedStatement book_state = con.prepareStatement(book_query);
+		    	book_state.setString(1, isbn);
+		    	book_state.setInt(2, i);
+		    	book_state.setString(3,location);
+		    	book_state.executeUpdate();
+		    	/* ALTERNATE WAY FOR REFERWENCE
+		    	String book_sql = "INSERT INTO BOOK_STOCK(isbn, copy_number, location)"
+		    			+ "VALUES('"+isbn+"','"+i+"','"+location+"')"; 
+		    	System.out.println(book_sql);
+
+				stmt.executeUpdate(book_sql);
+		    	 */
+	    	}
+    		System.out.println("Copies have succesfully been added to the database");
+
 		
 		}
 		catch(Exception e){
     		System.out.println("Something went wrong adding copies of books to database");
 		}
 		//Forces user to hit enter to return home
+		System.out.println("Please hit enter to return to the main menu");
     	in.nextLine();
     	return;
 	}
@@ -222,7 +289,7 @@ public class library_database {
 	    	String title = in.nextLine();
 	    	System.out.println("Enter author of new library book");
 	    	String author = in.nextLine();
-	    	System.out.println("Enter pulisher for new library book:");
+	    	System.out.println("Enter publisher for new library book:");
 	    	String publisher = in.nextLine();
 	    	System.out.println("Enter publication year of new library book:");
 	    	String pub_year = in.nextLine();
@@ -234,8 +301,9 @@ public class library_database {
 	    	String summary = in.nextLine();
 	    	
 	    	
-	    	String book_sql = "INSERT INTO BOOK_DIR(isbn, title, author, publisher, pub_year, format, book_subject, summary)"
+	    	String book_sql = "INSERT INTO BOOK_DIR(isbn, title, author, publisher, pub_year, book_format, book_subject, summary)"
 	    			+ "VALUES('"+isbn+"','"+title+"','"+author+"','"+publisher+"','"+pub_year+"','"+format+"','"+book_subject+"','"+summary+"')"; 
+	    	System.out.println(book_sql);
 
 			stmt.executeUpdate(book_sql);
 			System.out.println("*New book " + title +" has been added to the database. isbn: " + isbn + "*\n"
@@ -290,11 +358,9 @@ public class library_database {
     	ResultSet rs2=query2.executeQuery();
     	rs2.next();
     	int user_id = rs2.getInt(1) + 1;
-
     	
     	String user_sql = "INSERT INTO LIB_USER(user_id, username, uname, address, email, phone)"
     			+ "VALUES('"+user_id+"','"+username+"','"+name+"','"+address+"','"+email+"','"+phone+"')"; 
-
 		stmt.executeUpdate(user_sql);
 		System.out.println("*New user " + username +" has been added to the database. UserID: " + user_id + "*\n"
     				+ "please hit the enter key to return to main menu");
